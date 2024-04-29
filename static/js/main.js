66e6a32bb096041e52175c4b15b5eddbc0c5aa03
genwch <geo.wong@gmail.com>
2024-04-29 17:30:01 +0800

    upd

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 function getAllUrlParams(url) {
 
     url = (url||window.location.search.slice(1)).replace(/\\u0026/g, "&");
-
+    console.log(url);
     // get query string from url (optional) or window
     var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
   
