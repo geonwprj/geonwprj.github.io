80029afced81269b161afa171158570c7ac1a572
genwch <geo.wong@gmail.com>
2024-05-06 09:59:55 +0800

    upd

--- static/js/novel.js
+++ static/js/novel.js
@@ -1,3 +1,14 @@
+/* <main>
+<h1>Your title<h1>
+<div>Your subtitle</div>
+<div class="byline">
+By <a href="/">Your Name</a> -- 
+<time datetime="2018-04-06T05:52:50Z">April 6, 2018</time>
+</div>
+<p> article content... </p>
+<p> article content... </p>
+</main> */
+
 $(document).ready(() => {
     const title = getAllUrlParams().book;
     const index = getAllUrlParams().index;
@@ -7,6 +18,53 @@ $(document).ready(() => {
     console.log(url);
     const selector = "title,div.content"
 
+    let author = title.split("-")[1].trim();
+
+    let maindiv = document.createElement("main");
+    maindiv.id = "main";
+
+    let titlediv = document.createElement("h1");
+    titlediv.id = "titlediv";
+    let subtitlediv = document.createElement("div");
+    subtitlediv.id = "subtitlediv";
+
+    let byline = document.createElement("div");
+    byline.classList.add("byline");
+    byline.append(`By <a href="/">${author}</a> -- `);
+    let timediv = document.createElement("time");
+    timediv.setAttribute("datetime") = "";
+    timediv.append("");
+    byline.append(timediv);
+
+    maindiv.append(titlediv);
+    maindiv.append(subtitlediv);
+    maindiv.append(byline);
+
+    $("body").append(maindiv);
+
+    getScrape(url, selector).then(rtn => {
+      let chapter = rtn.result["title"][0].split("-")[0].trim();
+      chapter = chapter.split("-")[0].trim().replace(/⚡/g, "");
+      const chapterobj = chapter.split(" ");
+      let subchapter = chapterobj[1].trim();
+      for (var i=2; i<=chapterobj.length; i++) {
+        subchapter += " " + chapterobj[i];
+      }
+      chapter = chapterobj[0];
+  
+      $("head").append(`<meta property="og:title" content="${chapter} - ${subchapter}">`)
+      $("head").append(`<title>${chapter} - ${subchapter}</title>`);
+      $("#titlediv").append(chapter);
+      $("#subtitlediv").append(subchapter);
+
+      let novel = rtn.result["div.content"][0];
+      novel = splitText(novel).join("——");
+      $("#main").append(novel);
+
+    }).catch(e => console.error(e));
+
+
+    /*
     let contentdiv = document.createElement("article");
     let sectiondiv = document.createElement("section");
     let titlediv = document.createElement("div");
@@ -29,5 +87,5 @@ $(document).ready(() => {
       $("#section").append(novel);
 
     }).catch(e => console.error(e));
-
+    */
 })
