8fc429f604664d6e56c9a61b831c4903ea8d519e
genwch <geo.wong@gmail.com>
2024-05-06 10:29:12 +0800

    upd

--- static/js/novel.js
+++ static/js/novel.js
@@ -55,30 +55,4 @@ $(document).ready(() => {
       $("body").append(maindiv);  
 
     }).catch(e => console.error(e));
-
-
-    /*
-    let contentdiv = document.createElement("article");
-    let sectiondiv = document.createElement("section");
-    let titlediv = document.createElement("div");
-    let log = document.createElement("div");
-    titlediv.id = "title";
-    contentdiv.id = "content";
-    sectiondiv.id = "section";
-    contentdiv.append(sectiondiv);
-    log.id = "log";
-
-    $("body").append(titlediv);
-    $("body").append(contentdiv);
-    
-    getScrape(url, selector).then(rtn => {
-      let chapter = rtn.result["title"][0].split("-")[0].trim();
-      $("head").append(`<meta property="og:title" content="${chapter}">`)
-      $("head").append(`<title>${chapter}</title>`)
-      let novel = rtn.result["div.content"][0];
-      novel = splitText(novel).join("——");
-      $("#section").append(novel);
-
-    }).catch(e => console.error(e));
-    */
 })
