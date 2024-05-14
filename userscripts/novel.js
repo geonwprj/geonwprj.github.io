c7dbab911c2d987fc398589812cad39108c31298
genwch <geo.wong@gmail.com>
2024-05-14 14:50:58 +0800

    Update novel.js

--- userscripts/novel.js
+++ userscripts/novel.js
@@ -3,17 +3,45 @@
 // @description  Replace char in contents
 // @match        https://www.bg3.co/novel/pagea/*
 // @updateURL    https://raw.githubusercontent.com/geonwprj/geonwprj.github.io/main/userscripts/novel.js
-// @version      0.1
+// @version      0.2
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+    function getDetails() {
+        try {
+    	let title = document.getElementsByTagName('title')[0].textContent.split(' - ').slice(0,-1).join(' - ').trim().split(" ");
+    	let subtitle = title.slice(2).join(" ").trim();
+    	title = title[1].trim();
+            let currentURL = window.location.href;
+            let htmlPage = currentURL.split("/").slice(-1)[0].split(".")[0].split("_");
+            logging(JSON.stringify({
+        		"title": title,
+        		"subtitle": subtitle,
+                "author": htmlPage[0].split("-")[1],
+                "book": htmlPage[0].split("-")[0],
+                "index": htmlPage[1]
+            }));
+        } catch (e) {
+            logging(e);
+        }
+    }
+
     // Main function to run the script
     function main() {
-//        setDebug();
+        setDebug();
         logging("init"); // Fixed quotation marks
-        window.addEventListener('load', replaceCharInParagraphs);
+
+        if (document.readyState === 'complete') {
+            replaceCharInParagraphs();
+		    getDetails();
+        } else {
+            // Use DOMContentLoaded for better reliability
+            document.addEventListener('DOMContentLoaded', replaceCharInParagraphs);
+            document.addEventListener('DOMContentLoaded', getDetails);
+        }
+    	logging(`ready`);
     }
 
     // Function to log messages
@@ -48,12 +76,14 @@
 
     // Function to replace characters in <p> elements
     function replaceCharInParagraphs() {
-        var paragraphs = document.getElementsByTagName('p');
+	logging(`replaceCharInParagraphs - start`);
+       var paragraphs = document.getElementsByTagName('p');
         for (var i = 0; i < paragraphs.length; i++) {
             paragraphs[i].textContent = paragraphs[i].textContent.replace(/曰/g, '日');
-            paragraphs[i].textContent = paragraphs[i].textContent.replace(/……+/g, '……').replace(/——+/g, '——');
+            paragraphs[i].textContent = paragraphs[i].textContent.replace(/……+/g, '……').replace(/——+/g, '——').replace(/\s?[Wωшw]{3}(.*)[￠CC]\s?[ΟO0]\n/g, '');
         }
-    }
+	logging(`replaceCharInParagraphs - end`);
+   }
 
   // Run the main function
     main();
