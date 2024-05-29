1f8fec3d070f76990910c0eb33e48c3a5ba5c350
genwch <geo.wong@gmail.com>
2024-05-29 15:40:26 +0800

    Update novel.js

--- static/js/novel.js
+++ static/js/novel.js
@@ -60,7 +60,7 @@ function getSource(title, index, author, isjson) {
       
       let novel = rtn.result["div.content"][0];
       let novelTxt = splitText(novel);
-      let data = {"title": chapter, "subtitle": subchapter, "index": index, "content": novelTxt};
+      let data = {"title": chapter, "subtitle": subchapter, "index": index, "content": novelTxt[0].split("\n")};
       let mainpre = document.createElement("pre");
       mainpre.innerHTML = JSON.stringify(data, null, 2);
       $("body").append(mainpre);
