e323685a1adaa3727c9333cc18ea6c17d948eace
genwch <geo.wong@gmail.com>
2024-08-12 17:32:18 +0800

    upd

--- static/js/football.js
+++ static/js/football.js
@@ -14,9 +14,7 @@ function capitalizeWords(str) {
 async function getZhQuery(query) {
     const baseUrl = "https://en.wikipedia.org/wiki/";
     console.log(query);
-    console.log(query.replace(' ', '_'));
-    console.log(capitalizeWords(query))
-    console.log(capitalizeWords(query.replace(' ', '_')))
+    console.log(capitalizeWords(query).replace(' ', '_'))
     const searchUrl = baseUrl + capitalizeWords(query).replace(' ', '_');
 
     try {
@@ -25,8 +23,11 @@ async function getZhQuery(query) {
         const parser = new DOMParser();
         const doc = parser.parseFromString(text, 'text/html');
         const link = doc.querySelector('a[hreflang="zh"]');
+        console.log(link);
         return link ? link.href.replace('https://zh.wikipedia.org/wiki/', '') : capitalizeWords(query).replace(' ', '_');
     } catch (error) {
+        console.log('error');
+        console.log(error);
         return query.replace(' ', '_');
     }
 }
