6b6996d8bd9c428e46bf69fd938d267345a255b1
genwch <geo.wong@gmail.com>
2024-08-13 17:02:36 +0800

    upd

---
+++ static/js/stream.js
@@ -0,0 +1,16 @@
+async function fetchAndDisplayContent(url) {
+    try {
+        let response = await fetch(url);
+        if (response.ok) {
+            let data = await response.text();
+            document.getElementById('content').innerHTML = data;
+        } else {
+            console.error('Network response was not ok.');
+        }
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+}
+
+// Replace 'https://example.com/data' with the URL you want to fetch content from
+fetchAndDisplayContent('https://www.zhibo1.cc/list_Soccer_1.html');
