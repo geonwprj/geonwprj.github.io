f6819564a5c2b8a398161fb30e37a7b881f22464
genwch <geo.wong@gmail.com>
2024-08-13 09:57:09 +0800

    upd

--- static/js/football.js
+++ static/js/football.js
@@ -1,8 +1,9 @@
 $(document).ready(async () => {
     const name = getAllUrlParams().q;
-    const zh_name = await getZhQuery(name);
-    console.log(zh_name);
+    // const zh_name = await getZhQuery(name);
+    // console.log(zh_name);
     let mainpre = document.createElement("pre");
+    const zh_name = await scrapeWikipedia(name, 'Hong_Kong')
     mainpre.innerHTML = name + '<br>' + zh_name;
     $("body").append(mainpre);    
 })
@@ -25,38 +26,23 @@ async function scrapeWikipedia(name, region) {
     // Prepare Wikipedia search URL
     const zhName = await getZhQuery(name);
     const searchUrl = wikipediaUrls[region] + zhName;
-
+    selector = 'caption.fn'
+    const searchUrl = baseUrl + capitalizeWords(query).replace(' ', '_');
+    let rtn = await getScrape(searchUrl, selector)
+    console.log(rtn);
     try {
-        // Send a GET request to the Wikipedia page
-        const response = await fetch(searchUrl);
-        const text = await response.text();
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(text, 'text/html');
-
-        // Try to find the Chinese names in the infobox, if it exists
-        const infobox = doc.querySelector('table.infobox');
-        if (infobox) {
-            const zhRow = infobox.querySelector('caption.fn');
-            if (zhRow) {
-                const chineseName = zhRow.innerHTML.split('<br>')[0].trim();
-                return chineseName;
-            }
-        } else {
-            console.log('no infobox');
-        }
-    } catch (error) {
-        console.error('Error fetching Wikipedia page:', error);
+        return rtn.result[selector][0];
+    } catch {
+        return name
     }
-    return null;
 }
 
 async function getZhQuery(query) {
     const baseUrl = "https://en.wikipedia.org/wiki/";
-    console.log(query);
-    console.log(capitalizeWords(query).replace(' ', '_'))
+    // console.log(query);
+    // console.log(capitalizeWords(query).replace(' ', '_'))
     const searchUrl = baseUrl + capitalizeWords(query).replace(' ', '_');
     let rtn = await getScrape(searchUrl, 'a[lang="zh"]', 'href')
-    console.log(rtn);
+    // console.log(rtn);
     return rtn.result ? rtn.result.replace('https://zh.wikipedia.org/wiki/', '') : capitalizeWords(query).replace(' ', '_');
-
 }
