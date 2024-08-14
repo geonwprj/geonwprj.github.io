9c7808ada18053d448438cc030b6a26e71096b65
genwch <geo.wong@gmail.com>
2024-08-14 14:28:48 +0800

    upd

--- static/js/stream.js
+++ static/js/stream.js
@@ -1,10 +1,23 @@
 $(document).ready(async () => {
     const name = getAllUrlParams().q;
+    let cf_key = getAllUrlParams().key;
+
+    cf_key = 'MzJjZTNjZDU3NjNhM2Q2YzQ0ZjQyYzY4YjM0ODc2YzkuYWNjZXNzOjE2NzhhZWQ2YzliZjRmNzliOGUyNTk0ZGEzZTMwYjhlOGZkODEzNjFkYjM0NmFlMTE0ZTU3ZDUzNDk3ZjczZDg=';
+    cf_val = extractIdAndSecret(cf_key);
+    console.log(cf_val);
 
     let mainpre = document.createElement("pre");
-    let key = getQueryParam('x-api-key');
-    console.log(key);
-    mainpre.innerHTML = key;
+    mainpre.innerHTML = '';
     $("body").append(mainpre);    
 })
 
+function extractIdAndSecret(base64String) {
+    // Decode the Base64 string
+    const decodedString = atob(base64String);
+
+    // Split the decoded string by the delimiter ':'
+    const [id, secret] = decodedString.split(':');
+
+    // Return the extracted id and secret as an object
+    return { id, secret };
+}
