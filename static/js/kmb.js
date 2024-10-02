3e3c7af59f898ca97ac4b2ce59375e4c0d94d04e
genwch <geo.wong@gmail.com>
2024-10-02 11:24:44 +0800

    upd

--- static/js/kmb.js
+++ static/js/kmb.js
@@ -126,7 +126,7 @@ async function findRoutes(fromLat, fromLong, toLat, toLong) {
         return distA - distB; // Sort by total distance last
     });
 
-    console.log('All Results:', results); // Log all results
+    console.log('Found Routes:', results); // Log all results
 
     return results.slice(0, 30); // Return at most 30 records for display
 }
@@ -143,7 +143,6 @@ $(document).ready(async () => {
     $('body').append('<div id="results"></div>');
     
     findRoutes(fromLat, fromLong, toLat, toLong).then(routes => {
-        console.log('Found Routes:', routes);
 
         routes.forEach(v => {
             $("#results").append(`
