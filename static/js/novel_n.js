9e9b0687eaaa6690980e01b5e4cd5d9a9b2b33d1
genwch <geo.wong@gmail.com>
2024-12-27 14:27:20 +0800

    upd

--- static/js/novel_n.js
+++ static/js/novel_n.js
@@ -4,27 +4,30 @@ $(document).ready(async () => {
     // <a href="/book/3534941/1926994403.html">第1章 超神级天赋:天罚之手！</a>
     let title = getAllUrlParams().book;
     let id = getAllUrlParams().bookid;
-    const index = getAllUrlParams().index;
+    let index = getAllUrlParams().index;
     let mode = getAllUrlParams().mode;
     let source = getAllUrlParams().source;
     // https://web.scraper.workers.dev/?url=https%3A%2F%2Fwww.drxsw.com%2Fbook%2F3534941%2F1926994403.html&selector=a%23nextChapterBottom&scrape=attr&attr=href
     // https://web.scraper.workers.dev/?url=https%3A%2F%2Fwww.drxsw.com%2Fbook%2F3534941%2F1926994403.html&selector=div%23TextContent&scrape=text
     // a#nextChapterBottom
 
-    // source = source? source: "drxsw";
-    // if (source = "drxsw") {
-    //     let url = "https://www.drxsw.com/book/";
-    //     url += id + "/";
-    //     getPage(url);
-    // }
-
-    let url = "https://www.drxsw.com/book/3534941/1926994403.html";
-    let nextpage = await getNextUrl(url);
     console.log('xxz');
-    console.log(nextpage);
-    $("body").append(nextpage);
-    let content = await getContent(url);
-    console.log(content);
+    index = index? index: "1";
+    
+    source = source? source: "drxsw";
+    if (source = "drxsw") {
+        let url = "https://www.drxsw.com/zh_hant/book/";
+        url += id + "/";
+        let page = await getPage(url, index);
+        console.log(page);
+    }
+
+    // let url = "https://www.drxsw.com/zh_hant/book/3534941/1926994403.html";
+    // let nextpage = await getNextUrl(url);
+    // console.log(nextpage);
+    // $("body").append(nextpage);
+    // let content = await getContent(url);
+    // console.log(content);
 
   })
 
@@ -40,16 +43,8 @@ async function getContent(url) {
   return rtn.result[selector][0];
 }
 
-function getPage(url) {
-    return fetch(url).then((r) => {
-        if (r.status===200) {
-            console.log(t.text());
-          return r.text();
-        } else {
-          return {};
-        }
-      }).catch((e) => {
-        console.error(e);
-        return {};
-      });
+async function getPage(url, index) {
+  const selector = "div#newlist > ul#chapterList > li:nth-child(" + index + ") > a";
+  let rtn = await getScrape(url, selector, 'href');
+  return rtn.result;
 }
