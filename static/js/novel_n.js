2307f23dd08a0a68da468ab78f54b7ad58f3d0a1
genwch <geo.wong@gmail.com>
2024-12-27 14:41:56 +0800

    upd

--- static/js/novel_n.js
+++ static/js/novel_n.js
@@ -33,6 +33,12 @@ $(document).ready(async () => {
     nextlink.href = "/novel_n.html?bookid="+id+"&index="+parseInt(index)+1;
 
     let content = await getContent(url);
+
+    content = content.replace(/(AdProvider = window.AdProvider || []).push({"serve": {}});/g, "");
+    let lines = content.split(/\n\r/);
+    // Wrap each line with <p> tags and join them back into a single string
+    content = lines.map(line => `<p>${line}</p>`).join("");
+
     // console.log(content);
     let contentdiv = document.createElement("div");
     contentdiv.id = "TextContent"
