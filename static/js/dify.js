fbc67a8a843d85fe64f2b6293904db91c6dd307f
genwch <geo.wong@gmail.com>
2024-12-30 13:28:26 +0800

    upd

--- static/js/dify.js
+++ static/js/dify.js
@@ -6,10 +6,6 @@ async function runWorkflow(key, inputs) {
         "user": "geonwprj-github"
     };
 
-        // {
-        //     "input_text": "不僅如此。~||~到達修德里·白芸這個層次，她很清楚，要進一步提升實力，很難很難。~||~殺戮之神的無盡殺戮，雪帝今天破不了，即便再給雪帝幾年時間，他大概率也是破不了的。~||~正是因爲想清楚這些，修德里·白芸才選擇了果斷撤兵。"
-        // },
-
     try {
         const response = await fetch(url, {
             method: 'POST',
@@ -23,18 +19,13 @@ async function runWorkflow(key, inputs) {
             mode: 'no-cors'
         });
         
-        console.log(response);
-
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
 
         const data = await response.json();
-        console.log(data);
-        document.getElementById('api-response').textContent = JSON.stringify(data, null, 2);
+        return data.data.outputs.second_translation;
     } catch (error) {
-        console.log('!!!! error');
-        console.log(error);
         console.error('There was a problem with the fetch operation:', error);
     }
 }
